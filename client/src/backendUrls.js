0b224514007ac591d6fd1104624c021a3b704dc2
Mattia <mgirolami99@gmail.com>
2022-02-18 12:07:51 +0100

    Prove

--- client/src/backendUrls.js
+++ client/src/backendUrls.js
@@ -1,5 +1,6 @@
 const backendUrls = {
-  http: "http://localhost:4000",
+  //http: "http://localhost:4000",
+  http: "https://glacial-sierra-79322.herokuapp.com",
   ws: "/graphql",
   //ws: "ws://localhost:4000/graphql",
 };
